f4c79a31d855c0b5af31f05f6f866e8299fbe259
OperatorIO-stock <vova.chigorin@yandex.com>
2024-12-30 19:48:20 +0300

    commit G

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace local_max;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rangeNumberMax = 101;
+        int rangeNumberMin = 1;
+
+        int[] numbers = new int [13];
+        Random random = new Random();
+
+        int lastIndex = numbers.Length - 1;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(rangeNumberMin, rangeNumberMax);
+            Console.Write($"{numbers[i]} ");
+        }
+
+        Console.WriteLine("\n");
+
+        if (numbers[0] > numbers[1])
+            Console.WriteLine($"The local max: {numbers[0]}");
+
+        else
+            Console.WriteLine($"The local max: {numbers[1]}");
+
+        for (int i = 1; i < lastIndex - 1; i++)
+        {
+            if (numbers[i] > numbers[i - 1])
+            {
+                if (numbers[i] > numbers[i + 1])
+                    Console.WriteLine($"The local max: {numbers[i]}");
+            }
+        }
+
+        if (numbers.Length > 1)
+        {
+            if (numbers[lastIndex] > numbers[lastIndex - 1])
+                Console.WriteLine($"The local max: {numbers[lastIndex]}");
+            else
+                Console.WriteLine($"The local max: {numbers[lastIndex - 1]}");
+        }
+    }
+}
